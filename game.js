0b867236838c222160ebcd05b68e695088aad587
Keecreamer <keecreamer@optonline.net>
2014-11-04 15:09:57 -0500

    Experimenting with DOM and finding elements.

---
+++ game.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('action'));
+console.log(document.getElementsByTagName('h1'));
+console.log(document.querySelectorAll('aside'));
+console.log(document.querySelector("title"));
+console.log(document.getElementsByClassName('game'));
+console.log(document.querySelectorAll('body'));
+console.log(document.querySelectorAll('html > .children()'));
